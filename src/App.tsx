a140a141d1947288e0cdc3a882a6564c082745b3
kihoii <kihikolee@gmail.com>
2024-07-28 20:38:53 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import React from 'react';
-import { DatePicker } from 'antd';
-import { HomePage } from './pages/HomePage/HomePage.tsx';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+import React from "react";
+import { DatePicker } from "antd";
+import { HomePage } from "./pages/HomePage/HomePage.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return <div>
-    
-    <HomePage/>
-    </div>
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
-export default App
+export default App;
