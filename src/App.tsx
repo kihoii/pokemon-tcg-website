ce180e42d0f610db8828289d0e9741bc0cb4824c
kihoii <kihikolee@gmail.com>
2024-07-29 21:37:57 +0300

    nav moved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
-import React from "react";
-import { DatePicker } from "antd";
 import { HomePage } from "./pages/HomePage/HomePage.tsx";
+import { Navbar } from "./components/Navbar/Navbar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       <HomePage />
     </>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,5 @@
-import { Navbar } from "../../components/Navbar/Navbar";
 import "./HomePage.scss";
 
 export const HomePage = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <></>;
 };
