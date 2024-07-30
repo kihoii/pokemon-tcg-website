000f0ff970b63b65919ddc799ed899ed1ce6c5bc
Eugene.k <kulinich.eugene.2412@gmail.com>
2024-07-30 20:42:20 +0500

    add-pokemon-page asdsaasd

--- src/pages/CardsPage/CardsPage.tsx
+++ src/pages/CardsPage/CardsPage.tsx
@@ -1,57 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 import { Card, Col, Row } from "antd";
 import "./CardsPage.scss";
 
 const { Meta } = Card;
 
-interface QQQ {
-  images: string[];
+interface IPokemon {
+  images: {
+    small: string;
+  };
 }
 
 export function CardsPage(): React.JSX.Element {
-  const [image, setImage] = useState(
-    "https://images.pokemontcg.io/hgss4/1.png"
-  );
+  const [images, setImages] = useState<string[]>([]);
 
-  // useEffect(() => {
-  //   console.debug("effect");
-  // }, [image]);
+  useEffect(() => {
+    const getCards = async () => {
+      try {
+        let response = await fetch(`https://api.pokemontcg.io/v2/cards`, {
+          method: "GET",
+          headers: { "X-Api-Key": "cf7723da-bd7a-4b30-ad96-c24ce22fc106" },
+        });
+        let data = await response.json();
+        let cards = data.data as IPokemon[];
+        let imageUrls = cards.map((card) => card.images.small);
+        setImages(imageUrls);
+      } catch (error) {
+        console.error("Error fetching cards:", error);
+      }
+    };
 
-  const getCards = (): any => {
-    return fetch(`https://api.pokemontcg.io/v2/cards`, {
-      method: "GET",
-      headers: { "X-Api-Key": "cf7723da-bd7a-4b30-ad96-c24ce22fc106" },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        let qw = data.data as QQQ[];
-        let qwe = qw.map((x) => x.images as string[]);
-        let qq = qwe;
-
-        // setImage()
-      });
-  };
+    getCards();
+  }, []);
 
   return (
     <div>
-      {"" && getCards()}
       CardsPage
       <div>
         <Row gutter={16}>
-          <Col span={8}>
-            <Card
-              hoverable
-              title="Card title"
-              bordered={false}
-              cover={<img alt="qqq" src={image} />}
-            >
-              Card content
-              <Meta
-                title="Europe Street beat"
-                description="www.instagram.com"
-              />
-            </Card>
-          </Col>
+          {images.map((image, index) => (
+            <Col span={8} key={index}>
+              <Card
+                hoverable
+                title="Card title"
+                bordered={false}
+                cover={<img alt="Card image" src={image} />}
+              >
+                Card content
+                <Meta
+                  title="Europe Street beat"
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
     </div>
