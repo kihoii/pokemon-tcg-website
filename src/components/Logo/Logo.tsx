56ab09ae71562b0a6774b83eb9f6960956023456
kihoii <kihikolee@gmail.com>
2024-08-03 18:16:37 +0300

    home page positioning fixed

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,11 @@
-import "./Logo.scss";
-import { Link } from "react-router-dom";
-import logo from "./assets/logo.svg";
+import './Logo.scss';
+import { Link } from 'react-router-dom';
+import logo from './assets/logo.svg';
 
 export const Logo = () => {
   return (
     <Link to="">
-      <img src={logo} alt="Logo" />
+      <img className="logo-img" src={logo} alt="Logo" />
     </Link>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
-import { Hero } from "./components/Hero/Hero";
-import "./HomePage.scss";
+import { Hero } from './components/Hero/Hero';
+import { UserRating } from './components/Hero/UserRating/UserRating';
+import './HomePage.scss';
 
 export const HomePage = () => {
   return (
-    <>
+    <div id="home-page">
       <Hero />
-    </>
+    </div>
   );
 };
