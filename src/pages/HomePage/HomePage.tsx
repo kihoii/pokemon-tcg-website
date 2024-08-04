0ed5ee8038f354aa231443fb1f32856ddcf21f19
kihoii <kihikolee@gmail.com>
2024-08-04 21:00:08 +0300

    middle banner section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Hero } from './components/Hero/Hero';
 import { HowItWorks } from './components/HowItWorks/HowItWorks';
+import { MiddleBanner } from './components/MiddleBanner/MiddleBanner';
 import { SubscribeWidget } from './components/SubscribeWidget/SubscribeWidget';
 import './HomePage.scss';
 
@@ -7,6 +8,7 @@ export const HomePage = () => {
   return (
     <div id="home-page">
       <Hero />
+      <MiddleBanner />
       <HowItWorks />
       <SubscribeWidget />
     </div>
