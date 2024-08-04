25871fd8546a34b8100f5398e2ad92eaa34ce56c
kihoii <kihikolee@gmail.com>
2024-08-04 20:51:02 +0300

    hook form

--- src/components/SubscribeInput/SubscribeInput.tsx
+++ src/components/SubscribeInput/SubscribeInput.tsx
@@ -1,15 +1,34 @@
-import { Button, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import './SubscribeInput.scss';
 import envelope from './assets/EnvelopeSimple.svg';
+import { FormItem } from 'react-hook-form-antd';
+import { useForm } from 'react-hook-form';
 
 export const SubscribeInput = () => {
+  const { control, handleSubmit } = useForm();
+
   return (
     <div className="subscribe-input">
-      <Input type="email" placeholder="Enter your email here" />
-      <Button type="primary">
-        <img src={envelope} alt="" />
-        Subscribe
-      </Button>
+      <Form
+        name="email-subscribe"
+        onFinish={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <FormItem control={control} name="email" label="">
+          <Input
+            type="email"
+            placeholder="Enter your email here"
+            autoComplete="on"
+          />
+        </FormItem>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            <img src={envelope} alt="" />
+            Subscribe
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
