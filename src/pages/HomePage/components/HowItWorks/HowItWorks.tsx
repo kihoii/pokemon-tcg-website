e3a3ecf72e7eec1f1e3ad6540563b987336d0b46
kihoii <kihikolee@gmail.com>
2024-08-04 20:27:17 +0300

    how it works coloring fixed

--- src/pages/HomePage/components/HowItWorks/HowItWorks.tsx
+++ src/pages/HomePage/components/HowItWorks/HowItWorks.tsx
@@ -6,32 +6,38 @@ import chat from './assets/chat.svg';
 export const HowItWorks = () => {
   return (
     <section id="how-it-works" className="container">
-      <div className="info-card">
-        <div className="info-card-image">
-          <img src={cart} alt="" />
-        </div>
-        <div className="info-card-title">Покупай Новые Карточки</div>
-        <div className="info-card-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-          necessitatibus molestiae distinctio aliquid voluptatem
-        </div>
+      <div className="headline">
+        <h3>How It Works</h3>
+        <p>Find out how to get started</p>
       </div>
-      <div className="info-card">
-        <div className="info-card-image">
-          <img src={folder} alt="" />
+      <div className="info-cards-block">
+        <div className="info-card">
+          <div className="info-card-image">
+            <img src={cart} alt="" />
+          </div>
+          <div className="info-card-title">Покупай Новые Карточки</div>
+          <div className="info-card-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+            necessitatibus molestiae distinctio aliquid voluptatem
+          </div>
         </div>
-        <div className="info-card-title">Отслеживай Свою Коллекцию</div>
-        <div className="info-card-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <div className="info-card">
+          <div className="info-card-image">
+            <img src={folder} alt="" />
+          </div>
+          <div className="info-card-title">Отслеживай Свою Коллекцию</div>
+          <div className="info-card-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </div>
         </div>
-      </div>
-      <div className="info-card">
-        <div className="info-card-image">
-          <img src={chat} alt="" />
-        </div>
-        <div className="info-card-title">Узнавай Новости</div>
-        <div className="info-card-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <div className="info-card">
+          <div className="info-card-image">
+            <img src={chat} alt="" />
+          </div>
+          <div className="info-card-title">Узнавай Новости</div>
+          <div className="info-card-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </div>
         </div>
       </div>
     </section>
