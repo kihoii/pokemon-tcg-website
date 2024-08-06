d0f61ef2f569bf20e669a3945336aeb44bd4b930
Eugene.k <kulinich.eugene.2412@gmail.com>
2024-08-06 22:14:45 +0500

    test layout

--- src/api/helpers.tsx
+++ src/api/helpers.tsx
@@ -1,7 +1,7 @@
 import { PokemonDto } from '../interfaces/PokemonDto.tsx';
 import { BaseUrl, ApiKey } from './constants.tsx';
 
-const BaseGet = (url: string) => {
+const baseGet = (url: string) => {
   return fetch(BaseUrl + url, {
     method: 'GET',
     headers: { 'X-Api-Key': ApiKey },
@@ -10,11 +10,24 @@ const BaseGet = (url: string) => {
 
 export const getCards = async (): Promise<PokemonDto[]> => {
   try {
-    const response = await BaseGet(`cards`);
+    const response = await baseGet(`cards`);
     const data = await response.json();
     return data.data as PokemonDto[];
   } catch (error) {
     console.error('Error fetching cards:', error);
     return [];
+  }
+};
+
+export const getCardById = async (
+  id: string
+): Promise<PokemonDto | undefined> => {
+  try {
+    const response = await baseGet(`cards/` + id);
+    const data = await response.json();
+    return data.data as PokemonDto;
+  } catch (error) {
+    console.error('Error fetching cards:', error);
+    return;
   }
 };
