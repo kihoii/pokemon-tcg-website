eb514ea56bd89287e097e6600e5db3fa2c789731
Eugene.k <kulinich.eugene.2412@gmail.com>
2024-08-06 22:46:39 +0500

    add pokemon page content

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import './CardItem.css';
 import { Col, Card } from 'antd';
 import { PokemonDto } from '../../interfaces/PokemonDto.tsx';
-// import { PokemonPage } from '../../pages/PokemonPage/PokemonPage.tsx';
 
 const { Meta } = Card;
-
-function cqqqq() {
-  //  <PokemonPage key={id} pokemon={id}/>
-}
 
 export const CardItem: React.FC<{ pokemon: PokemonDto }> = ({ pokemon }) => (
   <Col span={6}>
     <Card
       hoverable={true}
-      onClick={cqqqq}
+      onClick={() => window.location.assign('/pokemon/' + pokemon.id)}
       className="pokemon-card"
       title={pokemon.name}
       bordered={false}
--- src/pages/PokemonPage/PokemonPage.tsx
+++ src/pages/PokemonPage/PokemonPage.tsx
@@ -1,1 +1,42 @@
 import './PokemonPage.scss';
+import { useQuery } from 'react-query';
+import { getCardById } from '../../api/helpers.tsx';
+import { useParams } from 'react-router-dom';
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+export function PokemonPage(): React.JSX.Element {
+  const params = useParams();
+  const {
+    data: card,
+    error,
+    isLoading,
+  } = useQuery(['card', params.id], () => getCardById(params.id as string));
+
+  if (isLoading) {
+    return <div className="loader">Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading cards</div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="cards-page">
+        <Card
+          className="pokemon-card"
+          title={card?.name}
+          bordered={false}
+          cover={
+            <img className="img" alt={card?.id} src={card?.images.large} />
+          }
+        >
+          {card?.rarity}
+          <Meta title={card?.artist} description={card?.flavorText} />
+        </Card>
+      </div>
+    </div>
+  );
+}
