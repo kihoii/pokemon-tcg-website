d3917b4d8c6d5ff92a5ec61eccfddd109cb16de8
Eugene.k <kulinich.eugene.2412@gmail.com>
2024-08-07 22:48:24 +0500

    add pagination

--- src/pages/CardsPage/CardsPage.tsx
+++ src/pages/CardsPage/CardsPage.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Row } from 'antd';
+import { useState } from 'react';
+import { Row, Pagination } from 'antd';
+import type { PaginationProps } from 'antd';
 import './CardsPage.scss';
 import { CardItem } from '../../components/CardItem/CardItem';
 import { getCards } from '../../api/helpers.tsx';
 import { useQuery } from 'react-query';
 
 export function CardsPage(): React.JSX.Element {
-  const { data: cards, error, isLoading } = useQuery('cards', getCards);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(8);
+  const {
+    data: cards,
+    error,
+    isLoading,
+  } = useQuery(['cards', page, pageSize], () => getCards(page, pageSize));
+
+  const onChange: PaginationProps['onChange'] = (pageNumber, pageSize) => {
+    setPage(pageNumber);
+    setPageSize(pageSize);
+  };
 
   if (isLoading) {
     return <div className="loader">Loading...</div>;
@@ -22,6 +35,14 @@ export function CardsPage(): React.JSX.Element {
         <Row gutter={16}>
           {cards?.map((item) => <CardItem key={item.id} pokemon={item} />)}
         </Row>
+        <Pagination
+          showQuickJumper
+          defaultCurrent={page}
+          pageSize={pageSize}
+          pageSizeOptions={[8, 16, 20]}
+          total={250}
+          onChange={onChange}
+        />
       </div>
     </div>
   );
