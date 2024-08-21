b8c1503a4e055195374bc93514a93289e55ac23f
Eugene.k <kulinich.eugene.2412@gmail.com>
2024-08-22 00:23:08 +0500

    add wishList's remove button

--- src/pages/PokemonPage/PokemonPage.tsx
+++ src/pages/PokemonPage/PokemonPage.tsx
@@ -3,12 +3,19 @@ import { useQuery } from 'react-query';
 import { getCardById } from '../../api/helpers.tsx';
 import { useParams } from 'react-router-dom';
 import { Button, Card } from 'antd';
-import { addToWishList } from '../../store/wishListSlice.tsx';
+import {
+  addToWishList,
+  removeFromWishList,
+} from '../../store/wishListSlice.tsx';
 import { useAppDispatch } from '../../store/hooks.tsx';
+import { LocalStorageService } from '../../services/localStorageService.tsx';
+import { pokemonWishList } from '../../store/localStorageKeys.tsx';
+import { useState, useEffect } from 'react';
 
 const { Meta } = Card;
 
 export function PokemonPage(): React.JSX.Element {
+  const [isInWishList, setIsInWishList] = useState(false);
   const dispatch = useAppDispatch();
   const params = useParams();
   const {
@@ -17,8 +24,19 @@ export function PokemonPage(): React.JSX.Element {
     isLoading,
   } = useQuery(['card', params.id], () => getCardById(params.id as string));
 
+  useEffect(() => {
+    const wishList = LocalStorageService.getItem(pokemonWishList) || [];
+    setIsInWishList(wishList.includes(params.id));
+  }, [params.id]);
+
   function onClickAddToWishList(cardId: string) {
     dispatch(addToWishList(cardId));
+    setIsInWishList(true);
+  }
+
+  function onClickRemoveFromWishList(cardId: string) {
+    dispatch(removeFromWishList(cardId));
+    setIsInWishList(false);
   }
 
   if (isLoading) {
@@ -41,12 +59,21 @@ export function PokemonPage(): React.JSX.Element {
           {card?.rarity}
           <Meta title={card?.artist} description={card?.flavorText} />
         </Card>
-        <Button
-          type="primary"
-          onClick={() => onClickAddToWishList(card?.id as string)}
-        >
-          Add to Wishlist
-        </Button>
+        {isInWishList ? (
+          <Button
+            danger
+            onClick={() => onClickRemoveFromWishList(card?.id as string)}
+          >
+            Remove from Wishlist
+          </Button>
+        ) : (
+          <Button
+            type="primary"
+            onClick={() => onClickAddToWishList(card?.id as string)}
+          >
+            Add to Wishlist
+          </Button>
+        )}
         <Button type="primary">Buy Card</Button>
       </div>
     </div>
