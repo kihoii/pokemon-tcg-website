f2b000a96f50640f479abb3e1bae7709caa2db80
kihoii <kihikolee@gmail.com>
2024-08-24 19:38:44 +0300

    form validation

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,91 +1,111 @@
 import './SignUpPage.scss';
 import placeholder from './Ivysaur.png';
 import type { FormProps } from 'antd';
-import { Button, Form, Input } from 'antd';
+import { Button, DatePicker, Form, Input } from 'antd';
 
 import { FormItem } from 'react-hook-form-antd';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import dayjs, { type Dayjs } from 'dayjs';
+import * as z from 'zod';
 
-type FieldType = {
-  username?: string;
-  email?: string;
-  password?: string;
-  passwordConfirm?: string;
-};
+const schema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'Required' })
+    .max(15, { message: 'Username should be less than 15 characters' }),
+  email: z.string({ message: 'Required' }),
+  birthDate: z.instanceof(dayjs as unknown as typeof Dayjs, {
+    message: 'Required',
+  }),
+  password: z.string().min(1, { message: 'Required' }),
+  passwordConfirm: z.string().min(1, { message: 'Required' }),
+});
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+const onFinish: FormProps['onFinish'] = (values) => {
+  values['birthDate'] = values['birthDate'].format('YYYY-MM-DD');
   console.log('Success:', values);
 };
 
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+export const SignUpPage = () => {
+  const { control, handleSubmit } = useForm({ resolver: zodResolver(schema) });
 
-export const SignUpPage = ({ name }: { name: string }) => {
-  const { control, handleSubmit } = useForm();
   return (
-    <div>
-      <section id="main-section">
-        <div className="left-container">
-          <img className="image-pokemon" src={placeholder} alt="placeholder" />
+    <section id="sign-up-page">
+      <div className="container">
+        <h2>Create an account</h2>
+        <div className="sign-up-box">
+          <img className="left-banner" src={placeholder} alt="placeholder" />
+          <div className="right-container">
+            <p className="text">
+              Welcome! Enter your details and start your Pokemon collection!
+              <br /> We hope you enjoy it!
+            </p>
+            <Form
+              name="sign-up-form"
+              onFinish={handleSubmit(onFinish)}
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600, borderRadius: 120 }}
+              initialValues={{ remember: true }}
+              autoComplete="off"
+            >
+              <FormItem control={control} name="name" label="">
+                <Input
+                  placeholder="Enter username"
+                  autoComplete="on"
+                  maxLength={16}
+                />
+              </FormItem>
+
+              <FormItem control={control} name="email" label="" required>
+                <Input
+                  type="email"
+                  placeholder="Enter email"
+                  autoComplete="on"
+                />
+              </FormItem>
+
+              <FormItem
+                control={control}
+                name="birthDate"
+                label="Date Of Birth"
+                required
+              >
+                <DatePicker />
+              </FormItem>
+
+              <FormItem control={control} name="password" label="" required>
+                <Input.Password
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  autoComplete="on"
+                />
+              </FormItem>
+
+              <FormItem
+                control={control}
+                name="passwordConfirm"
+                label=""
+                required
+              >
+                <Input.Password
+                  type="password"
+                  placeholder="Confirm password"
+                  autoComplete="on"
+                />
+              </FormItem>
+
+              <Form.Item>
+                <Button className="button" type="primary" htmlType="submit">
+                  Sign Up
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
         </div>
-        <div className="right-container">
-          <h1>Create an account</h1>
-          <h2 className="text">
-            Welcome! Enter your details and start collecting your Pokemon
-            collection! We hope you enjoy it!
-          </h2>
-          <Form
-            name={name}
-            onFinish={handleSubmit((data) => {
-              console.log(data);
-            })}
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600, borderRadius: 120 }}
-            initialValues={{ remember: true }}
-            autoComplete="off"
-          >
-            <FormItem control={control} name="username" label="">
-              <Input
-                type="Username"
-                placeholder="Please input your username!"
-                autoComplete="on"
-              />
-            </FormItem>
-
-            <FormItem control={control} name="email" label="">
-              <Input
-                type="email"
-                placeholder="Please input your Email Address!"
-                autoComplete="on"
-              />
-            </FormItem>
-
-            <FormItem control={control} name="password" label="">
-              <Input
-                type=""
-                placeholder="Please input your password!"
-                autoComplete="on"
-              />
-            </FormItem>
-
-            <FormItem control={control} name="passwordConfirm" label="">
-              <Input
-                type="password"
-                placeholder="Please input your password!"
-                autoComplete="on"
-              />
-            </FormItem>
-
-            <Form.Item>
-              <Button className="button" type="primary" htmlType="submit">
-                Зарегистрироваться
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
